d532e8c128eb5e83a9fd86edaff8d7cdfea043d0
worsleyj <worsleyj23@gmail.com>
2024-11-14 22:23:15 -0500

    remove commented out code

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,7 +3,6 @@ const indexRouter = Router();
 const queries = require("../controllers/indexController");
 
 indexRouter.get("/", (req, res) => {
-  // res.render("index");
   queries.getGraphicsCards(req, res);
 });
 
