b01272d4a31831aa02ed39f6f674cda6889cd6fd
worsleyj <worsleyj23@gmail.com>
2024-11-19 14:08:39 -0500

    router dynamic test

--- routes/partRouter.js
+++ routes/partRouter.js
@@ -6,6 +6,12 @@ partRouter.get("/", (req, res) => {
   parts.getParts(req, res);
 });
 
+const partNum = "/1";
+
+partRouter.get(partNum, (req, res) => {
+  parts.getParts(req, res);
+});
+
 partRouter.get("/create", (req, res) => {
   res.render("new_part");
 });
